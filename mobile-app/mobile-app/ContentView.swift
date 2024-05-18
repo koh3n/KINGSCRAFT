dca5cacacae031a1c1fca8e92f53d5a4414f7417
jaydenpiao <jaydenpiao@gmail.com>
2024-05-18 12:09:21 -0700

    init mobile app

---
+++ mobile-app/mobile-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile-app
+//
+//  Created by Jayden Piao on 2024-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile-app/mobile-app/mobile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_appApp.swift
+//  mobile-app
+//
+//  Created by Jayden Piao on 2024-05-18.
+//
+
+import SwiftUI
+
+@main
+struct mobile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
