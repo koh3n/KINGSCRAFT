b3d901dc6cf87577a6733fe0f6f71a37c265bde7
davidlim <daviddlim11@gmail.com>
2024-05-18 16:54:35 -0700

    some stuff

--- web-app/frontend/src/pages/Dashboard.jsx
+++ web-app/frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import GoogleLogin from '../components/GoogleLogin';
 import UserAccount from '../components/UserAccount';
+import { Swiper, SwiperSlide } from "swiper/react";
 import '../styles/Dashboard.css'
+import "swiper/css";
+
+import image1 from "./airpod.jpg";
+import image2 from "./chess.png";
+
+const slides = [image1, image2, image1, image2, image1, image2];
+
+const Projects = () => {
+  return (
+    <>  
+        <div className="project_container">
+            <Swiper spaceBetween={10} slidesPerView={3} loop>
+                {slides.map((slide) => (
+                    <SwiperSlide>
+                    <img src={slide} alt="Phone" />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    </>
+  );
+};
 
 const Dashboard = () => {
     return (
@@ -11,8 +34,8 @@ const Dashboard = () => {
                 <div className='button-cont'>
                     <UserAccount />
                     <GoogleLogin />
-                    <button>`</button>
                 </div>
+                    <Projects />
                 </div>
             </div>
         </>
