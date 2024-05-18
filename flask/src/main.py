db1aa47ade92aa1a9f4d32ca455872b5f519f95f
koh3n <kohenlee1234@gmail.com>
2024-05-18 15:20:42 -0700

    blueprint bad

--- flask/src/main.py
+++ flask/src/main.py
@@ -1,11 +1,36 @@
-from flask import Flask
-from src.routes.api_routes import ai_bp
-from src.routes.s3_routes import s3_bp
+from flask import Blueprint, Flask, request, jsonify
+from src.util.api_util import *
+from src.util.s3_util import *
+
 
 app = Flask(__name__)
 
-app.register_blueprint(ai_bp)
-app.register_blueprint(s3_bp)
+
+
+@app.route("/upload", methods = ['POST'])
+def get_image_route():
+    return get_image()
+
+@app.route("/process", methods = ['POST'])
+def process_image():
+    return preprocess()
+
+@app.route("/images", methods = ['POST'])
+def image_route():
+    data = request.get_json()
+
+    if data is None:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+    if 'username' not in data:
+        return jsonify({"error": "No username provided"}), 400
+
+    username = data['username']
+
+    return get_all_images(username), 200
+
+
+
 
 if __name__ == '__main__':
         app.run(debug=True)
