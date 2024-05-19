ccc08b8af9f496b06688bd135e4799066ccea0c3
jaydenpiao <jaydenpiao@gmail.com>
2024-05-19 11:26:40 -0700

    DONE

--- web-app/frontend/src/components/DisplayItem.jsx
+++ web-app/frontend/src/components/DisplayItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/DisplayItem.css';
 import { get_object, parse_url } from '../util/utils';
 import { Link } from 'react-router-dom';
@@ -7,14 +7,25 @@ import firebase from '../firebase/firebase';
 const DisplayItem = ({ imageUrl }) => {
   const name = parse_url(imageUrl);
   console.log(name);
+  const [url, setUrl] = useState("");
+  // get a list of model urls
+  // loop through the list and find the model url that matches the image
+  // and set the model url to the url state
+  // return a tag with model url as href
+
+
   // console.log("url", imageUrl);
-  const url = get_object(firebase.auth().currentUser.uid, name);  
-  console.log(url);
+
+
+  get_object(firebase.auth().currentUser.uid, name).then((result)=>{setUrl(result); console.log(url);}, console.log("failed to retrieve model url"));
+
   return (
     <div className="display-item">
-      <Link to={url}>
-        <img src={imageUrl} alt="Display" />
-      </Link>
+      
+        <a href={url}>
+          <img src={imageUrl} alt="Display" />
+        </a>
+      
     </div>
   );
 };
