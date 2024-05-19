ec0c60b6709129ab7b254e2faab1d18e4ae383a7
jaydenpiao <jaydenpiao@gmail.com>
2024-05-18 21:14:17 -0700

    3.0 underway

--- mobile-app-2.0/app/_layout.js
+++ mobile-app-2.0/app/_layout.js
@@ -1,6 +1,13 @@
 import Slot from "expo-router/Slot";
+import { View, Text } from "react-native";
 
 // Import your global CSS file
 import "../global.css";
 
-export default Slot;
+export default function _layout() {
+  return (
+    <View>
+      <Slot />
+    </View>
+  );
+}
