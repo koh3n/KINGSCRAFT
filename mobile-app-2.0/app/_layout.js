5e664ea59e6783b81eb44ce87ff6caaeb85c0393
jaydenpiao <jaydenpiao@gmail.com>
2024-05-18 20:26:15 -0700

    fixed nativewind

---
+++ mobile-app-2.0/app/_layout.js
@@ -0,0 +1,6 @@
+import Slot from "expo-router/Slot";
+
+// Import your global CSS file
+import "../global.css";
+
+export default Slot;
--- mobile-app-2.0/tailwind.config.js
+++ mobile-app-2.0/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
-  ],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
